e2066f0e503df79a200b50ca750baae588231d64
Nadezhda <99nadegda99@mail.ru>
2020-08-16 19:05:11 +0700

    Thirst

---
+++ Task6/Task6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool find_value(ref int [] arr, int target_value)
+            {
+                int right = arr.Length - 1;
+                int left = 0;
+
+                while (true)
+                {
+                    int value = (left + right) / 2;
+
+                    if (value == target_value)
+                    {
+                        return true;
+                    }
+                    else if (value < target_value)
+                    {
+                        left = value;
+                    }
+                    else
+                    {
+                        right = value; 
+                    }
+
+                    if (right == left + 1)
+                    {
+                        return false;
+                    } 
+                }
+            }
+
+
+            Console.WriteLine("Введите кол-во элементов в массиве: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr_ = new int [count];
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i = 0; i < count; i++)
+            {
+                arr_[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите значение, которое нужно найти в массиве: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(find_value(ref arr_, number));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int find_min_sort(ref int[] arr)
+            {
+                return arr[0];
+            }
+
+            int find_min(ref int[] arr)
+            {
+                int min_elem = arr[0];
+                
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] < min_elem)
+                    {
+                        min_elem = arr[i];
+                    }
+                }
+
+                return min_elem; 
+            }
+
+
+            Console.WriteLine("Введите кол-во элементов в массиве: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr1 = new int[count];
+            Console.WriteLine("Введите элементы в массив в порядке возрастания: ");
+            for (int i = 0; i < count; i++)
+            {
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Минимум: " + find_min_sort(ref arr1));
+
+            Console.WriteLine("Введите кол-во элементов в массиве: ");
+            int count2 = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr2 = new int[count2];
+            Console.WriteLine("Введите элементы в циклически сдвинутый массив: ");
+            for (int i = 0; i < count2; i++)
+            {
+                arr2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Минимум: " + find_min(ref arr2));
+
+            Console.ReadKey();
+        }
+
+    }
+}
